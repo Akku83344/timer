ea1896e24ac7326d0eccf2d760beb48de5d434cd
Akku83344 <akku83344@gmail.com>
2023-07-04 00:58:35 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import NewsPage from "./pages/NewsLetter";
+import "./App.css";
 import { Home } from "./pages/Home";
 import Timer from "./pages/Timer";
 
@@ -16,4 +17,3 @@ function App() {
 }
 
 export default App;
-
